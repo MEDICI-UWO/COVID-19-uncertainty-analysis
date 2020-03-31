6874452f69d9302158c519e203a7c5d4f2c90f6a
Eunice Chan <echan295@uwo.ca>
2020-03-30 21:38:09 -0400

    updated files in preparation to push to GitHub

--- Matlab/cases_CFR_top3_2020_month.m
+++ Matlab/cases_CFR_top3_2020_month.m
@@ -10,56 +10,8 @@ CMAP = jet(4);
 %% FIGURE
 figure
 
-%% CHINA
-ax1 = subplot(1, 4, 1);
-China = data(data.Country == "China", :);
-top3 = China(China.Year == 2020, :).Val/12;
-y = make_y_vector(xmin, xmax, top3, N);
-[X, Y] = meshgrid(x, y);
-Z = X.*Y;
-level = [min(Z(:)); top3];
-contourf(X, Y, Z, level, 'LineStyle', 'none');
-set(gca, 'YScale', 'log')
-caxis([min(level), max(level)]);
-cmap = revise_cmap(CMAP(1:length(level), :), level);
-colormap(gca, cmap)
-hold on;
-contour(X, Y, Z, [5e4, 1e5, 5e5], 'LineColor', 'w', 'LineWidth', 1);
-text(0.0215, 2.45e6, '50k', 'Color', 'w')
-text(0.021, 4.9e6, '100k', 'Color', 'k')
-text(0.042, 8.75e6, '500k', 'Color', 'w')
-text(0.028, 6.5e6, {'\leftarrow Stroke'}, 'Color', 'k')
-text(0.0425, 3.6e6, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
-text(0.0455, 2e6, '\leftarrow COPD', 'Color', 'k')
-title('China')
-hold off;
-
-%% USA
-ax2 = subplot(1, 4, 2);
-USA = data(data.Country == "United States", :);
-top3 = USA(USA.Year == 2020, :).Val/12;
-y = make_y_vector(xmin, xmax, top3, N);
-[X, Y] = meshgrid(x, y);
-Z = X.*Y;
-level = [min(Z(:)); top3];
-contourf(X, Y, Z, level, 'LineStyle', 'none');
-set(gca, 'YScale', 'log')
-caxis([min(level), max(level)]);
-cmap = revise_cmap(CMAP(1:length(level), :), level);
-colormap(gca, cmap)
-hold on;
-contour(X, Y, Z, [1e4, 5e4, 1e5], 'LineColor', 'w', 'LineWidth', 1);
-text(0.0205, 0.5e6, '10k', 'Color', 'w');
-text(0.022, 2.38e6, '50k', 'Color', 'w');
-text(0.043, 2.38e6, '100k', 'Color', 'w');
-text(0.0285, 1.7e6, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
-text(0.0267, 0.8e6, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
-text(0.038, 0.42e6, {'\leftarrow Lung', '     cancer'}, 'Color', 'k')
-title('USA')
-hold off;
-
 %% ITALY
-ax3 = subplot(1, 4, 3);
+ax1 = subplot(2, 3, 1);
 Italy = data(data.Country == "Italy", :);
 top3 = Italy(Italy.Year == 2020, :).Val/12;
 y = make_y_vector(xmin, xmax, top3, N);
@@ -78,11 +30,116 @@ text(0.0245, 4.125e5, '10k', 'Color', 'w')
 text(0.0275, 3e5, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
 text(0.0365, 1.7e5, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
 text(0.054, 1e5, {'\leftarrow Stroke'}, 'Color', 'k')
-title('Italy')
 hold off;
+title('Italy')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
+
+%% SPAIN
+ax2 = subplot(2, 3, 2);
+Spain = data(data.Country == "Spain", :);
+top3 = Spain(Spain.Year == 2020, :).Val/12;
+y = make_y_vector(xmin, xmax, top3, N);
+[X, Y] = meshgrid(x, y);
+Z = X.*Y;
+level = [min(Z(:)); top3];
+contourf(X, Y, Z, level, 'LineStyle', 'none');
+set(gca, 'YScale', 'log')
+caxis([min(level), max(level)]);
+cmap = revise_cmap(CMAP(1:length(level), :), level);
+colormap(gca, cmap)
+hold on;
+contour(X, Y, Z, [1e3, 5e3, 1e4], 'LineColor', 'w', 'LineWidth', 1);
+text(0.0205, 0.5e5, '1k', 'Color', 'w')
+text(0.024, 2.22e5, '5k', 'Color', 'w')
+text(0.046, 2.22e5, '10k', 'Color', 'w')
+text(0.0275, 1.6e5, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
+text(0.04, 1e5, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
+text(0.046, 0.6e5, {'\leftarrow Stroke'}, 'Color', 'k')
+hold off;
+title('Spain')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
+
+%% UNITED KINGDOM
+ax3 = subplot(2, 3, 3);
+UK = data(data.Country == "United Kingdom", :);
+top3 = UK(UK.Year == 2020, :).Val/12;
+y = make_y_vector(xmin, xmax, top3, N);
+[X, Y] = meshgrid(x, y);
+Z = X.*Y;
+level = [min(Z(:)); top3];
+contourf(X, Y, Z, level, 'LineStyle', 'none');
+set(gca, 'YScale', 'log')
+caxis([min(level), max(level)]);
+cmap = revise_cmap(CMAP(1:length(level), :), level);
+colormap(gca, cmap)
+hold on;
+contour(X, Y, Z, [5e3, 1e4], 'LineColor', 'w', 'LineWidth', 1);
+text(0.022, 2.48e5, '5k', 'Color', 'k')
+text(0.0245, 4.05e5, '10k', 'Color', 'w')
+text(0.0275, 2.9e5, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
+text(0.0355, 1.6e5, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
+text(0.042, 1e5, {'\leftarrow Stroke'}, 'Color', 'k')
+hold off;
+title('United Kingdom')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
+
+%% CHINA
+ax4 = subplot(2, 3, 4);
+China = data(data.Country == "China", :);
+top3 = China(China.Year == 2020, :).Val/12;
+y = make_y_vector(xmin, xmax, top3, N);
+[X, Y] = meshgrid(x, y);
+Z = X.*Y;
+level = [min(Z(:)); top3];
+contourf(X, Y, Z, level, 'LineStyle', 'none');
+set(gca, 'YScale', 'log')
+caxis([min(level), max(level)]);
+cmap = revise_cmap(CMAP(1:length(level), :), level);
+colormap(gca, cmap)
+hold on;
+contour(X, Y, Z, [5e4, 1e5, 5e5], 'LineColor', 'w', 'LineWidth', 1);
+text(0.0215, 2.45e6, '50k', 'Color', 'w')
+text(0.021, 4.9e6, '100k', 'Color', 'k')
+text(0.042, 8.75e6, '500k', 'Color', 'w')
+text(0.0275, 6.7e6, {'\leftarrow Stroke'}, 'Color', 'k')
+text(0.0425, 3.6e6, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
+text(0.0455, 2e6, '\leftarrow COPD', 'Color', 'k')
+hold off;
+title('China')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
+
+%% USA
+ax5 = subplot(2, 3, 5);
+USA = data(data.Country == "United States", :);
+top3 = USA(USA.Year == 2020, :).Val/12;
+y = make_y_vector(xmin, xmax, top3, N);
+[X, Y] = meshgrid(x, y);
+Z = X.*Y;
+level = [min(Z(:)); top3];
+contourf(X, Y, Z, level, 'LineStyle', 'none');
+set(gca, 'YScale', 'log')
+caxis([min(level), max(level)]);
+cmap = revise_cmap(CMAP(1:length(level), :), level);
+colormap(gca, cmap)
+hold on;
+contour(X, Y, Z, [1e4, 5e4, 1e5], 'LineColor', 'w', 'LineWidth', 1);
+text(0.0205, 0.5e6, '10k', 'Color', 'w');
+text(0.022, 2.38e6, '50k', 'Color', 'w');
+text(0.043, 2.38e6, '100k', 'Color', 'w');
+text(0.0285, 1.72e6, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
+text(0.0267, 0.8e6, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
+text(0.038, 0.42e6, {'\leftarrow Lung', '     cancer'}, 'Color', 'k')
+hold off;
+title('United States')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
 
 %% CANADA
-ax4 = subplot(1, 4, 4);
+ax6 = subplot(2, 3, 6);
 Canada = data(data.Country == "Canada", :);
 top3 = Canada(Canada.Year == 2020, :).Val/12;
 y = make_y_vector(xmin, xmax, top3, N);
@@ -97,26 +154,55 @@ colormap(gca, cmap)
 hold on;
 contour(X, Y, Z, [1e3, 5e3, 1e4], 'LineColor', 'w', 'LineWidth', 1);
 text(0.0205, 0.5e5, '1k', 'Color', 'w')
-text(0.0235, 2.22e5, '5k', 'Color', 'w')
-text(0.046, 2.22e5, '10k', 'Color', 'w')
+text(0.0235, 2.225e5, '5k', 'Color', 'w')
+text(0.046, 2.225e5, '10k', 'Color', 'w')
 text(0.0275, 1.6e5, {'\leftarrow Ischemic', '     heart disease'}, 'Color', 'k')
 text(0.029, 0.75e5, {"\leftarrow Alzheimer's", '     disease'}, 'Color', 'k')
 text(0.0445, 0.4e5, {"\leftarrow Lung", '     cancer'}, 'Color', 'k')
-title('Canada')
 hold off;
+title('Canada')
+xticks(linspace(xmin, xmax, 5));
+xticklabels({2, 4, 6, 8, 10});
 
 %% Axes titles
+
+% set(gcf, 'Position', [100, 100, 750, 850])
+
 p1 = get(ax1, 'position');
 p2 = get(ax2, 'position');
 p3 = get(ax3, 'position');
 p4 = get(ax4, 'position');
-width = p4(1) + p4(3) - p1(1);
-ax5 = axes('position', [p1(1) p1(2) width p1(4)], 'visible', 'off');
-ax5.XLabel.Visible = 'on';
-ax5.YLabel.Visible = 'on';
+p5 = get(ax5, 'position');
+p6 = get(ax6, 'position');
 
-axes(ax5)
-xlabel('Case Fatality Rate');
+y_translate = (p1(2) - (p4(2) + p4(4)))/4;
+
+p1(2) = p1(2) - y_translate;
+p2(2) = p2(2) - y_translate;
+p3(2) = p3(2) - y_translate;
+
+p1(4) = p1(4) + y_translate;
+p2(4) = p2(4) + y_translate;
+p3(4) = p3(4) + y_translate;
+p4(4) = p4(4) + y_translate;
+p5(4) = p5(4) + y_translate;
+p6(4) = p6(4) + y_translate;
+
+set(ax1, 'position', p1);
+set(ax2, 'position', p2);
+set(ax3, 'position', p3);
+set(ax4, 'position', p4);
+set(ax5, 'position', p5);
+set(ax6, 'position', p6);
+
+width = p3(1) + p3(3) - p1(1);
+height = p1(2) + p1(4) - p4(2);
+ax7 = axes('position', [p4(1) p4(2) width height], 'visible', 'off');
+ax7.XLabel.Visible = 'on';
+ax7.YLabel.Visible = 'on';
+
+axes(ax7)
+xlabel('Case Fatality Rate (%)');
 ylabel('Number of Confirmed Cases');
 
-set(gcf, 'Position', [100, 100, 1000, 525])
+set(gcf, 'Position', [100, 100, 750, 850])
--- R/predict_values.R
+++ R/predict_values.R
@@ -16,135 +16,170 @@ predict.value <- function(country.df, cause.string, degree, df) {
   cause.df <- country.df %>%
     filter(cause == cause.string) %>%
     arrange(year)
-  plot(cause.df$year, cause.df$val)
+  plot(cause.df$year, cause.df$val, xlab = 'Year', ylab = cause.string)
   reg <- lm(val~poly(year, degree = degree, raw = TRUE), data = cause.df)
-  print(summary(reg))
+  s <- summary(reg)
   order_id <- order(cause.df$year)
   lines(x = cause.df$year[order_id], y = fitted(reg)[order_id], col = 'red')
+  text(2017, min(cause.df$val), bquote(R^2~"="~.(s$r.squared)), adj = c(0.5, NA))
   return(suppressWarnings(predict(object = reg, newdata = df)))
 }
 
+# Function to get top 3 causes of death from Country data frame
+get.top3.causes <- function(country.df) {
+  top3 <- country.df %>%
+    filter(year == 2017) %>%
+    arrange(desc(val)) %>%
+    top_n(3)
+  top3 <- top3$cause
+  return(top3)
+}
+
 # Vectors to create new data frame with extrapolated data
-Country <- c(rep('Canada', times = 9), rep('Italy', times = 9), rep('United States', times = 9), rep('China', times = 9))
-Cause <- c()
-Year <- rep(df$year, times = 12)
-Val <- c()
+Country <- c(rep('Canada', times = 9), rep('Italy', times = 9), rep('United States', times = 9), rep('China', times = 9), rep('Spain', times = 9), rep('United Kingdom', times = 9))
+Cause <- NULL
+Year <- rep(df$year, times = 18)
+Val <- NULL
 
 ## CANADA
 Canada <- data %>% 
   filter(metric == 'Number', location == 'Canada')
 
-p <- predict.value(Canada, 'Ischemic heart disease', 4, df)
+Canada.top3 <- get.top3.causes(Canada)
+
+p <- predict.value(Canada, Canada.top3[1], 4, df)
 # 1        2        3 
 # 50219.33 52553.98 55287.88 
-Cause <- append(Cause, rep('Ischemic heart disease', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Canada.top3[1], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(Canada, "Alzheimer's disease and other dementias", 2, df)
+p <- predict.value(Canada, Canada.top3[2], 2, df)
 # 1        2        3 
 # 26020.49 26848.32 27697.67
-Cause <- append(Cause, rep("Alzheimer's disease and other dementias", times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Canada.top3[2], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(Canada, 'Tracheal, bronchus, and lung cancer', 1, df)
+p <- predict.value(Canada, Canada.top3[3], 1, df)
 # 1        2        3 
 # 21711.42 21950.71 22190.00
-Cause <- append(Cause, rep('Tracheal, bronchus, and lung cancer', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Canada.top3[3], times = 3))
+Val <- append(Val, p)
 
 ## ITALY
 Italy <- data %>% 
   filter(metric == 'Number', location == 'Italy')
+Italy.top3 <- get.top3.causes(Italy)
 
-p <- predict.value(Italy, 'Ischemic heart disease', 4, df)
+p <- predict.value(Italy, Italy.top3[1], 4, df)
 # 1         2         3 
 # 98771.58 100358.92 102254.80 
-Cause <- append(Cause, rep('Ischemic heart disease', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Italy.top3[1], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(Italy, "Alzheimer's disease and other dementias", 1, df)
+p <- predict.value(Italy, Italy.top3[2], 1, df)
 # 1        2        3 
 # 74323.86 75794.15 77264.44 
-Cause <- append(Cause, rep("Alzheimer's disease and other dementias", times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Italy.top3[2], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(Italy, 'Stroke', 4, df)
+p <- predict.value(Italy, Italy.top3[3], 4, df)
 # 1        2        3 
 # 61161.49 62478.23 64002.90
-Cause <- append(Cause, rep('Ischemic stroke', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(Italy.top3[3], times = 3))
+Val <- append(Val, p)
 
 ## USA
 USA <- data %>% 
   filter(metric == 'Number', location == 'United States')
+USA.top3 <- get.top3.causes(USA)
 
-p <- predict.value(USA, 'Ischemic heart disease', 4, df)
+p <- predict.value(USA, USA.top3[1], 4, df)
 # 1        2        3 
 # 546097.0 566830.1 591717.0
-Cause <- append(Cause, rep('Ischemic heart disease', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(USA.top3[1], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(USA, "Alzheimer's disease and other dementias", 1, df)
+p <- predict.value(USA, USA.top3[2], 1, df)
 # If using degree 1
 # 1        2        3 
 # 258038.5 262908.1 267777.7 
-Cause <- append(Cause, rep("Alzheimer's disease and other dementias", times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(USA.top3[2], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(USA, 'Tracheal, bronchus, and lung cancer', 4, df)
+p <- predict.value(USA, USA.top3[3], 4, df)
 # 1        2        3 
 # 190198.1 194428.8 199286.3 
-Cause <- append(Cause, rep('Tracheal, bronchus, and lung cancer', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(USA.top3[3], times = 3))
+Val <- append(Val, p)
 
 ## CHINA
 China <- data %>% 
   filter(metric == 'Number', location == 'China')
+China.top3 <- get.top3.causes(China)
 
-p <- predict.value(China, 'Stroke', 2, df)
+p <- predict.value(China, China.top3[1], 2, df)
 # 1       2       3 
 # 2104847 2137927 2171212
-Cause <- append(Cause, rep('Ischemic stroke', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(China.top3[1], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(China, 'Ischemic heart disease', 4, df)
+p <- predict.value(China, China.top3[2], 4, df)
 # 1       2       3 
 # 1825607 1868277 1906159
-Cause <- append(Cause, rep('Ischemic heart disease', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(China.top3[2], times = 3))
+Val <- append(Val, p)
 
-p <- predict.value(China, 'Chronic obstructive pulmonary disease', 4, df)
+p <- predict.value(China, China.top3[3], 4, df)
 # 1         2         3 
 # 989958.6 1031532.6 1083034.2 
-Cause <- append(Cause, rep('Chronic obstructive pulmonary disease', times = 3))
-for (i in 1:length(p)) {
-  Val <- append(Val, p[[i]])
-}
+Cause <- append(Cause, rep(China.top3[3], times = 3))
+Val <- append(Val, p)
+
+## SPAIN
+Spain <- data %>% 
+  filter(metric == 'Number', location == 'Spain')
+Spain.top3 <- get.top3.causes(Spain)
+
+p <- predict.value(Spain, Spain.top3[1], 2, df)
+
+Cause <- append(Cause, rep(Spain.top3[1], times = 3))
+Val <- append(Val, p)
+
+p <- predict.value(Spain, Spain.top3[2], 1, df)
+
+Cause <- append(Cause, rep(Spain.top3[2], times = 3))
+Val <- append(Val, p)
+
+p <- predict.value(Spain, Spain.top3[3], 4, df)
+
+Cause <- append(Cause, rep(Spain.top3[3], times = 3))
+Val <- append(Val, p)
+
+## UNITED KINGDOM
+UK <- data %>% 
+  filter(metric == 'Number', location == 'United Kingdom')
+UK.top3 <- get.top3.causes(UK)
+
+p <- predict.value(UK, UK.top3[1], 4, df)
+
+Cause <- append(Cause, rep(UK.top3[1], times = 3))
+Val <- append(Val, p)
+
+p <- predict.value(UK, UK.top3[2], 2, df)
+
+Cause <- append(Cause, rep(UK.top3[2], times = 3))
+Val <- append(Val, p)
+
+p <- predict.value(UK, UK.top3[3], 4, df)
+ 
+Cause <- append(Cause, rep(UK.top3[3], times = 3))
+Val <- append(Val, p)
+
+names(Val) <- NULL
 
 # Create new data frame with extrapolated data
 extrap.df = data.frame(Country = Country, Cause = Cause, Year = Year, Val = Val)
+
 # Write to csv file
 write.csv(extrap.df, 'Data/top3_extrapolation.csv')
 
